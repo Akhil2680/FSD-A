275de2e4bd56fadf957bceb3f3c8bfe833cdd64d
Akhil2680 <singhakhil52141@gmail.com>
2024-10-15 15:37:59 +0530

    javascript

---
+++ day5/script.js
@@ -0,0 +1,11 @@
+// let disp = () => {
+//     alert("welcome to arrow function in js");
+//     console.log("hi");
+// };
+// disp();
+
+let sum = (x, y) => {
+    return x + y;
+};
+let res = sum(12, 32);
+console.log(res);
